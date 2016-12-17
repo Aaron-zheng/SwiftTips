d77cc08f4c499e787b283a31e1d73f379b8bd62f
Aaron-zheng <fucheng.zheng@gmail.com>
2016-12-18 09:42:05 +1300

    update for common class and blur effect

---
+++ swifttips/swifttips/Constant.swift
@@ -0,0 +1,18 @@
+//
+//  Constant.swift
+//  swifttips
+//
+//  Created by Aaron on 18/12/2016.
+//  Copyright © 2016 sightcorner. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public let redColor = UIColor(red: 237 / 255.0, green: 65 / 255.0, blue: 45 / 255.0, alpha: 1.0)
+public let blueColor = UIColor(red: 62 / 255.0, green: 130 / 255.0, blue: 247 / 255.0, alpha: 1.0)
+public let greenColor = UIColor(red: 45 / 255.0, green: 169 / 255.0, blue: 79 / 255.0, alpha: 1.0)
+public let yellowColor = UIColor(red: 253 / 255.0, green: 189 / 255.0, blue: 0.0, alpha: 1.0)
+public let greyColor = UIColor(red: 238 / 255.0, green: 238 / 255.0, blue: 238 / 255.0, alpha: 1.0)
+public let darkColor = UIColor(red: 33/255, green: 33/255, blue: 33/255, alpha: 1)
+
